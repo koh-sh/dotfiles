e6bd661d8788fd2303c31d2a3aeacf293b9ffe4a
koh-sh <k@koh-sh.com>
2024-06-02 23:07:02 +0900

    fix and refactor

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,13 +4,16 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- configのlua実行にエラーがあった場合に出力する
+config:set_strict_mode(true)
+
 -- テーマ
 config.color_scheme = 'Adventure'
 
 -- フォント
+config.font = wezterm.font('JetBrains Mono', { weight = 'ExtraBold', italic = false })
 config.font_size = 12.2
-config.font =
-    wezterm.font('JetBrains Mono', { weight = 'ExtraBold', italic = false })
+
 -- 背景の非透過率（1なら完全に透過させない）
 config.window_background_opacity = 0.70
 
@@ -20,45 +23,40 @@ config.notification_handling = "NeverShow"
 -- キーバインド
 config.keys = {
     -- ¥ではなく、バックスラッシュを入力する。おそらくMac固有
-    {
-        key = "¥",
-        action = wezterm.action.SendKey { key = '\\' }
-    },
+    { key = "¥", mods = 'NONE',        action = wezterm.action.SendKey { key = '\\' } },
+
     -- cmd w でペインを閉じる（デフォルトではタブが閉じる）
-    {
-        key = "w",
-        mods = "CMD",
-        action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
+    { key = "w", mods = "SUPER",       action = wezterm.action.CloseCurrentPane { confirm = true } },
+
     -- cmd d で右方向にペイン分割
-    {
-        key = "d",
-        mods = "CMD",
-        action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } },
-    },
+    { key = "d", mods = "SUPER",       action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+
     -- cmd Shift d で下方向にペイン分割
-    {
-        key = "d",
-        mods = "CMD|SHIFT",
-        action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } },
-    },
+    { key = "d", mods = "SUPER|SHIFT", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+
     -- cmd Ctrl oでペインの中身を入れ替える
-    {
-        key = "o",
-        mods = "CMD|CTRL",
-        action = wezterm.action.RotatePanes 'Clockwise'
-    },
-    -- cmd []でペインの移動 (Prev)
-    {
-        key = '[',
-        mods = 'CMD',
-        action = wezterm.action.ActivatePaneDirection 'Prev',
-    },
-    {
-        key = ']',
-        mods = 'CMD',
-        action = wezterm.action.ActivatePaneDirection 'Next',
-    },
+    { key = "o", mods = "SUPER|CTRL",  action = wezterm.action.RotatePanes 'Clockwise' },
+
+    -- cmd []でペインの移動 (Prev/Next)
+    { key = '[', mods = 'SUPER',       action = wezterm.action.ActivatePaneDirection 'Prev' },
+    { key = ']', mods = 'SUPER',       action = wezterm.action.ActivatePaneDirection 'Next' },
+}
+
+-- searchのキーマッピング
+-- デフォルトは `wezterm show-keys --lua --key-table search_mode` で出力
+config.key_tables = {
+    search_mode = {
+        { key = 'Enter',     mods = 'NONE', action = wezterm.action.CopyMode 'PriorMatch' },
+        { key = 'Escape',    mods = 'NONE', action = wezterm.action.CopyMode 'Close' },
+        { key = 'n',         mods = 'CTRL', action = wezterm.action.CopyMode 'NextMatch' },
+        { key = 'p',         mods = 'CTRL', action = wezterm.action.CopyMode 'PriorMatch' },
+        { key = 'r',         mods = 'CTRL', action = wezterm.action.CopyMode 'CycleMatchType' },
+        { key = 'u',         mods = 'CTRL', action = wezterm.action.CopyMode 'ClearPattern' },
+        { key = 'PageUp',    mods = 'NONE', action = wezterm.action.CopyMode 'PriorMatchPage' },
+        { key = 'PageDown',  mods = 'NONE', action = wezterm.action.CopyMode 'NextMatchPage' },
+        { key = 'UpArrow',   mods = 'NONE', action = wezterm.action.CopyMode 'PriorMatch' },
+        { key = 'DownArrow', mods = 'NONE', action = wezterm.action.CopyMode 'NextMatch' },
+    }
 }
 
 -- and finally, return the configuration to wezterm
