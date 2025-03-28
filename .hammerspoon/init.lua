c752889e048e87940b563b039df7da3847aaa02d
koh-sh <k@koh-sh.com>
2025-03-28 23:41:34 +0900

    chore: refactor hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,59 +1,90 @@
--- Download Zip and execute to store under Spoon dir
--- https://github.com/Hammerspoon/Spoons/raw/master/Spoons/FnMate.spoon.zip
+-- Constants for key codes
+local KEY_CODES = {
+    LEFT_COMMAND = 0x37,
+    RIGHT_COMMAND = 0x36,
+    EISUU = 0x66,
+    KANA = 0x68
+}
+
+-- Load FnMate Spoon for function key handling
+-- Download from: https://github.com/Hammerspoon/Spoons/raw/master/Spoons/FnMate.spoon.zip
 hs.loadSpoon("FnMate")
 
-local function keyCode(key, modifiers)
-   modifiers = modifiers or {}
-   return function()
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-      hs.timer.usleep(1000)
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
-   end
-end
-
-local function remapKey(modifiers, key, keyCode)
-   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
-end
-
-remapKey({'ctrl'}, 'h', keyCode('left'))
-remapKey({'ctrl'}, 'j', keyCode('down'))
-remapKey({'ctrl'}, 'k', keyCode('up'))
-remapKey({'ctrl'}, 'l', keyCode('right'))
-remapKey({'ctrl', 'shift'}, 'h', keyCode('left', {'shift'}))
-remapKey({'ctrl', 'shift'}, 'j', keyCode('down', {'shift'}))
-remapKey({'ctrl', 'shift'}, 'k', keyCode('up', {'shift'}))
-remapKey({'ctrl', 'shift'}, 'l', keyCode('right', {'shift'}))
-remapKey({'ctrl', 'cmd'}, 'h', keyCode('left', {'cmd'}))
-remapKey({'ctrl', 'cmd'}, 'j', keyCode('down', {'cmd'}))
-remapKey({'ctrl', 'cmd'}, 'k', keyCode('up', {'cmd'}))
-remapKey({'ctrl', 'cmd'}, 'l', keyCode('right', {'cmd'}))
-remapKey({'ctrl', 'shift', 'cmd'}, 'h', keyCode('left', {'shift', 'cmd'}))
-remapKey({'ctrl', 'shift', 'cmd'}, 'j', keyCode('down', {'shift', 'cmd'}))
-remapKey({'ctrl', 'shift', 'cmd'}, 'k', keyCode('up', {'shift', 'cmd'}))
-remapKey({'ctrl', 'shift', 'cmd'}, 'l', keyCode('right', {'shift', 'cmd'}))
-remapKey({"ctrl"}, "f", keyCode("delete"))
-
-local prevKeyCode
-local leftCommand = 0x37
-local rightCommand = 0x36
-local eisuu = 0x66
-local kana = 0x68
-
-local function keyStroke(modifiers, character)
-    hs.eventtap.keyStroke(modifiers, character)
-end
-
-function HandleEvent(e)
-    local keyCode = e:getKeyCode()
-    local isCmdKeyUp = not(e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
-    if isCmdKeyUp and prevKeyCode == leftCommand then
-        keyStroke({}, eisuu)
-    elseif isCmdKeyUp and prevKeyCode == rightCommand then
-        keyStroke({}, kana)
+-- Helper function to create a key press function
+local function createKeyPressFunction(key, modifiers)
+    modifiers = modifiers or {}
+    return function()
+        hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+        hs.timer.usleep(1000)
+        hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
     end
-    prevKeyCode = keyCode
 end
 
-Eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, HandleEvent)
-Eventtap:start()
+-- Helper function to bind a key combination to a specific action
+local function bindKeyRemap(modifiers, key, keyCode)
+    hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
 
+-- Configure Vim-style arrow key mappings
+local function setupVimStyleNavigation()
+    -- Basic navigation
+    bindKeyRemap({ 'ctrl' }, 'h', createKeyPressFunction('left'))
+    bindKeyRemap({ 'ctrl' }, 'j', createKeyPressFunction('down'))
+    bindKeyRemap({ 'ctrl' }, 'k', createKeyPressFunction('up'))
+    bindKeyRemap({ 'ctrl' }, 'l', createKeyPressFunction('right'))
+
+    -- With Shift (for selection)
+    bindKeyRemap({ 'ctrl', 'shift' }, 'h', createKeyPressFunction('left', { 'shift' }))
+    bindKeyRemap({ 'ctrl', 'shift' }, 'j', createKeyPressFunction('down', { 'shift' }))
+    bindKeyRemap({ 'ctrl', 'shift' }, 'k', createKeyPressFunction('up', { 'shift' }))
+    bindKeyRemap({ 'ctrl', 'shift' }, 'l', createKeyPressFunction('right', { 'shift' }))
+
+    -- With Command (for word-level movement)
+    bindKeyRemap({ 'ctrl', 'cmd' }, 'h', createKeyPressFunction('left', { 'cmd' }))
+    bindKeyRemap({ 'ctrl', 'cmd' }, 'j', createKeyPressFunction('down', { 'cmd' }))
+    bindKeyRemap({ 'ctrl', 'cmd' }, 'k', createKeyPressFunction('up', { 'cmd' }))
+    bindKeyRemap({ 'ctrl', 'cmd' }, 'l', createKeyPressFunction('right', { 'cmd' }))
+
+    -- With Command + Shift (for word-level selection)
+    bindKeyRemap({ 'ctrl', 'shift', 'cmd' }, 'h', createKeyPressFunction('left', { 'shift', 'cmd' }))
+    bindKeyRemap({ 'ctrl', 'shift', 'cmd' }, 'j', createKeyPressFunction('down', { 'shift', 'cmd' }))
+    bindKeyRemap({ 'ctrl', 'shift', 'cmd' }, 'k', createKeyPressFunction('up', { 'shift', 'cmd' }))
+    bindKeyRemap({ 'ctrl', 'shift', 'cmd' }, 'l', createKeyPressFunction('right', { 'shift', 'cmd' }))
+
+    -- Additional mappings
+    bindKeyRemap({ "ctrl" }, "f", createKeyPressFunction("delete"))
+end
+
+-- IME switching functionality
+local function setupIMESwitching()
+    local prevKeyCode
+
+    local function keyStroke(modifiers, character)
+        hs.eventtap.keyStroke(modifiers, character)
+    end
+
+    local function handleIMEEvent(e)
+        local keyCode = e:getKeyCode()
+        local isCmdKeyUp = not (e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
+
+        if isCmdKeyUp then
+            if prevKeyCode == KEY_CODES.LEFT_COMMAND then
+                keyStroke({}, KEY_CODES.EISUU)
+            elseif prevKeyCode == KEY_CODES.RIGHT_COMMAND then
+                keyStroke({}, KEY_CODES.KANA)
+            end
+        end
+
+        prevKeyCode = keyCode
+    end
+
+    local eventTap = hs.eventtap.new(
+        { hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp },
+        handleIMEEvent
+    )
+    eventTap:start()
+end
+
+-- Initialize all configurations
+setupVimStyleNavigation()
+setupIMESwitching()
