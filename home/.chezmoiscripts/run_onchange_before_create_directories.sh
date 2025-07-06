5a481838ba5a35a50574f5c9e054cd3b8395ca35
koh-sh <k@koh-sh.com>
2025-07-06 15:06:17 +0900

    chore: refactor

--- home/.chezmoiscripts/run_onchange_before_create_directories.sh
+++ home/.chezmoiscripts/run_onchange_before_create_directories.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
-XDG_CONFIG_HOME="${HOME}/.config"
-XDG_CACHE_HOME="${HOME}/.cache"
-XDG_DATA_HOME="${HOME}/.local/share"
-XDG_STATE_HOME="${HOME}/.local/state"
-XDG_RUNTIME_DIR="/run/user/$UID"
+set -euo pipefail
 
-mkdir -p "$XDG_CACHE_HOME"/zsh
-mkdir -p "$XDG_STATE_HOME"/zsh
-mkdir -p "$XDG_DATA_HOME"/ansible
-mkdir -p "$XDG_DATA_HOME"/ansible/cp
-mkdir -p "$XDG_CONFIG_HOME"/aws/
-mkdir -p "$XDG_DATA_HOME"/go
-mkdir -p "$XDG_CONFIG_HOME/ipython"
-mkdir -p "$XDG_STATE_HOME"/less/
-mkdir -p "$XDG_CACHE_HOME"/vim
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
+XDG_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}"
+XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$UID}"
+
+readonly DIRECTORIES=(
+    "$XDG_CACHE_HOME/zsh"
+    "$XDG_STATE_HOME/zsh"
+    "$XDG_DATA_HOME/ansible"
+    "$XDG_DATA_HOME/ansible/cp"
+    "$XDG_CONFIG_HOME/aws"
+    "$XDG_DATA_HOME/go"
+    "$XDG_CONFIG_HOME/ipython"
+    "$XDG_STATE_HOME/less"
+    "$XDG_CACHE_HOME/vim"
+)
+
+for dir in "${DIRECTORIES[@]}"; do
+    mkdir -p "$dir"
+done
